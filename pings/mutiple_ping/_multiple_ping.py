1754ebe482cebc89543ef5d18b497eae6936d84a
QuintanilhaDev <mateusthequintanilha@gmail.com>
2023-01-23 10:52:49 -0300

    Testing yellow cat

--- pings/mutiple_ping/_multiple_ping.py
+++ pings/mutiple_ping/_multiple_ping.py
@@ -4,6 +4,7 @@ import time #Tempo de execução
 with open('hosts.txt') as file: #Com a abertura do "hosts.txt" como arquivo
     dump = file.read() #Ler o arquivo e jogar os dados dentro do dump
     dump = dump.splitlines() #Organizar as linhas na horizontal
+    print('Dumping...')
     for ip in dump: #Para cada IP no dump
         os.system('ping -c 2 {}'.format(ip)) #Apresenta o Ping de cada host
         time.sleep(5) #Intervalo de 5 segundos para cada Ping
